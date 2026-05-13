bd5a338842b662df9a8e0995848de7cedecb890c
Dikshya Chgn <np03cs4a240026@heraldcollege.edu.np>
2026-05-13 23:39:36 +0545

    Add subscription expired feature to check if subscription is expired or not

---
+++ public/subscription_expired.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Subscription Expired</title>
+</head>
+<body style="font-family:Arial; text-align:center; padding:100px;">
+
+<h1>Your Subscription Has Expired!!</h1>
+
+<p>Please renew your plan to continue accessing the system.</p>
+
+<a href="subscription_plans.php">Renew Now</a>
+
+</body>
+</html>
